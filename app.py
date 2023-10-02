c2cb80f0ea050d28d0f810a2c077a04992065dd8
pavannagula <npavankumar36@gmail.com>
2023-10-02 10:56:18 -0400

    Web Page Added

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import spacy
 
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,12 +12,22 @@ def extract_entities():
     # Get user input from the form
     user_input = request.form['user_input']
 
-    # Replace this with your NER model logic to extract entities
-    custom_ner = spacy.load("model-best") 
-    extracted_entities = custom_ner(user_input)
+    # Load your custom NER model
+    custom_ner = spacy.load("model-best")
+    
+    # Apply your NER model to the user's input
+    text_doc = custom_ner(user_input)
+
+    # Extract entities
+    extracted_entities = []
+    for ent in text_doc.ents:
+        extracted_entities.append({
+            'text': ent.text,
+            'label': ent.label_
+        })
 
     # Return the extracted entities as JSON
-    return ({'entities': extracted_entities})
+    return jsonify({'entities': extracted_entities})
 
 if __name__ == '__main__':
     app.run(debug=True)
